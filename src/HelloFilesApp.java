7cde82c194cd16d991260fbeaf85eae8a79a00fb
MiUsuario <github@abelardolg.com>
2025-02-19 20:02:59 +0100

    Ready to read the content of text files

--- src/HelloFilesApp.java
+++ src/HelloFilesApp.java
@@ -1,2 +1,19 @@
+import texto.TextFileManager;
+
+import java.util.Iterator;
+import java.util.List;
+
 public class HelloFilesApp {
+    public static void main(String[] args) {
+        texto();
+    }
+
+    private static void texto() {
+        List<String> data = TextFileManager.read();
+        Iterator<String> itLines = data.iterator();
+        while(itLines.hasNext()) {
+            String line = itLines.next();
+            System.out.println(line);
+        }
+    }
 }
